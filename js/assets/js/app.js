b6e0f991ac960d3bbc3218697a88740c78c12ca3
Jurianz <42801635+Jurianz@users.noreply.github.com>
2018-09-17 11:15:21 +0200

    folder changes

--- js/assets/js/app.js
+++ js/assets/js/app.js
@@ -1,1 +1,15 @@
-console.log('hij doet het');
+const firstName = 'Jurian',
+    lastName = 'Zweemer',
+    age = '17';
+
+
+const priceOfAnApple = '0.59',
+    numberOfApples = '200';
+const TotalPrice = priceOfAnApple * numberOfApples;
+console.log(`aantal apples: ${TotalPrice}`);
+
+const textMessage = `Please visit the HZ University of applied science`;
+console.log(textMessage);
+const textMessageReplace = textMessage.replace('applied','Mad');
+console.log(textMessageReplace);
+
